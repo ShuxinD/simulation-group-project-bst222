cfc0467007e239e1e4e6c9c1e14b9f9bb1a287be
ShuxinD <43856307+ShuxinD@users.noreply.github.com>
2022-11-29 15:09:39 -0500

    scenario2.1 2.2 simulation coding and results

--- measurementErrorSimulation.R
+++ measurementErrorSimulation.R
@@ -27,18 +27,18 @@ ideal$bp <- 0.14*ideal$truePM + 127 # the relationship is assumed to be 127 + 0.
 
 
 ## SCENARIO 1 ----
-#' measurement errors that are the same across season
-### generate measurement errors ----
+## measurement errors that are the same across season
+### generate error-prone pm25 ----
 ## only work on classical measurement error here
 set.seed(3)
 cesd <- 2; cd <- rnorm(10000, mean=0, sd=cesd) # get classical error, change the sd of classical error (try 1,2 and 3?)
 ideal$errorPM_scen1 <- ideal$truePM + cd
-### simulation
-set.seed(13)
+
+### simulation ----
 sampleSize <- c(100, 500, 1000, 2000)
 
-pmVal <- NA
-coeffMat <- NA
+pmVal <- NULL
+coeffMat <- NULL
 scen1Result <- data.frame()
 n_iter <- 500
 
@@ -81,100 +81,106 @@ fwrite(scen1Result, file.path("results", "scen1Results.csv"))
 
 ## SCENARIO 2 ----
 ### 2.1 additive ----
-### 2.2 multiplicative and additive ----
-
-
-# oriDat <- data.frame(bp, truePM, season)
-# scen3Dat <- data.frame()
-# 
-# seasonErr <- c(1, 1.5, 2, 2.5)
-# slopes <- c(1.9, 0.5, 0.8, 1.2)
-# 
-# for ( s in 1:4) {
-#   
-#   sbset <- dplyr::filter(oriDat, season == s)
-#   error <- rnorm(n = dim(sbset)[1], mean=0, sd = seasonErr[s])
-#   sbset$errorPM <- sbset$truePM*slopes[s]+error
-#   scen3Dat <- rbind(scen3Dat, sbset)
-#   
-# }
-
-
-
-# SCENARIO 2&3: measurement error vary by season (additive error w/wo multiplicative)
-
-oriDat <- data.frame(bp, truePM, season)
-scen3Dat <- data.frame()
-
-seasonErr <- c(1, 1.5, 2, 2.5)
-
-# for additive only, change all the slopes to 1
-slopes <- c(1.9, 0.5, 0.8, 1.2)
-
-for ( s in 1:4) {
-  
-  sbset <- dplyr::filter(oriDat, season == s)
-  error <- rnorm(n = dim(sbset)[1], mean=0, sd = seasonErr[s])
-  sbset$errorPM <- sbset$truePM*slopes[s]+error
-  scen3Dat <- rbind(scen3Dat, sbset)
-  
+## additive error
+sErr_sd <- c(1, 1.5, 2, 2.5)
+#### generate error-prone data ----
+errorPM_scen21 <- NULL
+for (s in 1:4) {
+  truePM_s <- ideal[season==s, truePM]
+  set.seed(s)
+  error <- rnorm(length(truePM_s), mean = 0, sd = sErr_sd[s]) # additive
+  errorPM_s <- truePM_s + error # multiplicative
+  errorPM_scen21 <- c(errorPM_scen21, errorPM_s)
 }
+ideal <- cbind(ideal, errorPM_scen21)
 
-
-
-
-## simulation for scenario 2&3
-set.seed(888)
-
-scen3Result <- data.frame()
+#### simulation ----
+sampleSize <- c(100, 500, 1000, 2000)
+scen21Result <- data.frame()
 
 for (siz in 1:4) {
-  
-  coeffMat <- matrix(ncol = 5, nrow = 500)
-  
-  
+  coeffMat <- matrix(ncol = 4, nrow = 500)
   for (iter in 1:500) {
-    
     print(iter)
-    
+    set.seed(iter)
     sampS <- sample(c(1:10000), size = sampleSize[siz])
-    subDat <- scen3Dat[sampS,]
-    
-    # regression calibration models
-    model1 <- lm(truePM~errorPM, data=subDat)
-    model2 <- lm(truePM~errorPM+as.factor(season), data=subDat)
-    model3 <- lm(truePM~errorPM*as.factor(season), data=subDat)
+    subDat <- ideal[sampS,]
+    ## regression calibration models
+    model1 <- lm(truePM~errorPM_scen21, data=subDat)
+    model2 <- lm(truePM~errorPM_scen21+as.factor(season), data=subDat)
+    model3 <- lm(truePM~errorPM_scen21*as.factor(season), data=subDat)
     ## predict the calibrated PM2.5 from different regression calibration methods
-    pmVal <- data.frame(predict(model1), predict(model2), predict(model3), subDat$truePM, subDat$errorPM)
-    
-    for (rc in 1:5) {
+    pmVal <- cbind(predict(model1, newdata = ideal),
+                   predict(model2, newdata = ideal), 
+                   predict(model3, newdata = ideal), 
+                   ideal$errorPM_scen21)
+    for (rc in 1:4) {
       # estimate the association between systolic blood pressure and 
       # calibrated pm2.5, true pm2.5 and error-prone pm2.5
-      model4 <- lm(subDat$bp~pmVal[,rc])
+      model4 <- lm(ideal$bp~pmVal[,rc])
       coeffMat[iter, rc] <- model4$coefficients[2]
-      
-      
     }
-    
-    
   } 
-  
-  coeff <- as.data.frame(coeffMat)
-  colnames(coeff) <-c("regCal1", "regCal2", "regCal3", "truePM", "errorPM")
-  coeff$sampleSize <- sampleSize[siz]
-  
-  scen3Result <- rbind(scen3Result, coeff)
-  
-  
+  coeff <- cbind(coeffMat, sampleSize[siz])
+  scen21Result <- rbind(scen21Result, coeff)
 }
+setDT(scen21Result)
+colnames(scen21Result) <-c("regCal1", "regCal2", "regCal3", "errorPM_scene1", "sampleSize")
+View(scen21Result)
+## create results directory
+ifelse(dir.exists(file.path("results")), "results dir exists", dir.create(file.path("results")))
+## save simulation results
+fwrite(scen21Result, file.path("results", "scen21Result.csv"))
+### 2.2 multiplicative and additive ----
+## additive error
+sErr_sd <- c(1, 1.5, 2, 2.5)
+## multiplicative error
+sErr_slopes <- c(1.9, 0.5, 0.8, 1.2) # for additive only, change all the slopes to 1
+#### generate error-prone data ----
+errorPM_scen22 <- NULL
+for (s in 1:4) {
+  truePM_s <- ideal[season==s, truePM]
+  set.seed(s)
+  error <- rnorm(length(truePM_s), mean = 0, sd = sErr_sd[s]) # additive
+  errorPM_s <- truePM_s*sErr_slopes[s] + error # multiplicative
+  errorPM_scen22 <- c(errorPM_scen22, errorPM_s)
+}
+ideal <- cbind(ideal, errorPM_scen22)
 
+#### simulation ----
+sampleSize <- c(100, 500, 1000, 2000)
+scen22Result <- data.frame()
 
-check <- dplyr::filter(scen3Result, sampleSize==2000)
-summary(check)
-
-
-
-
-
-
-
+for (siz in 1:4) {
+  coeffMat <- matrix(ncol = 4, nrow = 500)
+  for (iter in 1:500) {
+    print(iter)
+    set.seed(iter)
+    sampS <- sample(c(1:10000), size = sampleSize[siz])
+    subDat <- ideal[sampS,]
+    ## regression calibration models
+    model1 <- lm(truePM~errorPM_scen22, data=subDat)
+    model2 <- lm(truePM~errorPM_scen22+as.factor(season), data=subDat)
+    model3 <- lm(truePM~errorPM_scen22*as.factor(season), data=subDat)
+    ## predict the calibrated PM2.5 from different regression calibration methods
+    pmVal <- cbind(predict(model1, newdata = ideal),
+                   predict(model2, newdata = ideal), 
+                   predict(model3, newdata = ideal), 
+                   ideal$errorPM_scen22)
+    for (rc in 1:4) {
+      # estimate the association between systolic blood pressure and 
+      # calibrated pm2.5, true pm2.5 and error-prone pm2.5
+      model4 <- lm(ideal$bp~pmVal[,rc])
+      coeffMat[iter, rc] <- model4$coefficients[2]
+    }
+  } 
+  coeff <- cbind(coeffMat, sampleSize[siz])
+  scen22Result <- rbind(scen22Result, coeff)
+}
+setDT(scen22Result)
+colnames(scen22Result) <-c("regCal1", "regCal2", "regCal3", "errorPM_scene1", "sampleSize")
+View(scen22Result)
+## create results directory
+ifelse(dir.exists(file.path("results")), "results dir exists", dir.create(file.path("results")))
+## save simulation results
+fwrite(scen22Result, file.path("results", "scen22Result.csv"))
